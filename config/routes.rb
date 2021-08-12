7d68a1c69827248abf44c3beb221953f18f6eda7
Kevin Konczak <konczakkevin@gmail.com>
2021-08-12 16:57:19 +0900

    forgot about the destroy option. Added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'bookmarks/create'
   resources :lists, only: %i[index new show create] do
-    resources :bookmarks, only: %i[new create]
+    resources :bookmarks, only: %i[new create destroy]
   end
 end
