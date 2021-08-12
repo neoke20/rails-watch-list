2ab256a0748f8b47c0236a8ac91f3be0d6b1daa4
Kevin Konczak <konczakkevin@gmail.com>
2021-08-12 16:23:22 +0900

    finished the bookmark. Still need to style the pages better

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
- get 'lists', to: 'lists#index', as: :lists
- get 'lists/new', to: 'lists#new', as: :new_list
- get 'lists/:id', to: 'lists#show', as: :list
- post 'lists', to: 'lists#create'
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  resources :lists, only: %i[index new show create] do
+    resources :bookmarks, only: %i[new create]
+  end
 end
