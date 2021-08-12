c19785e8147fbe2d64be5a37414555bd4e53c996
Kevin Konczak <konczakkevin@gmail.com>
2021-08-12 23:47:29 +0900

    added a destroy method for lists and changed style of lists cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'bookmarks/create'
-  resources :lists, only: %i[index new show create] do
+  resources :lists, only: %i[index new show create destroy] do
     resources :bookmarks, only: %i[new create destroy]
   end
 end
