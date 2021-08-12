f654404216dd03e659f7dc9e70e5882ecb84ca83
Kevin Konczak <konczakkevin@gmail.com>
2021-08-12 15:26:48 +0900

    added new and create list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
  get 'lists', to: 'lists#index', as: :lists
+ get 'lists/new', to: 'lists#new', as: :new_list
  get 'lists/:id', to: 'lists#show', as: :list
+ post 'lists', to: 'lists#create'
 end
