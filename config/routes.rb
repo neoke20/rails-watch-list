a78e72e4138671e00baab07561c93e31b2b10ced
Kevin Konczak <konczakkevin@gmail.com>
2021-08-13 15:03:25 +0900

    rerouted the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # get 'bookmarks/new'
-  # get 'bookmarks/create'
+  root to: 'lists#index'
   resources :lists, only: %i[index new show create destroy] do
     resources :bookmarks, only: %i[new create destroy]
   end
