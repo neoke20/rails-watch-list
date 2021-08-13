3c641dfe20f5ed03945a36de52e19371748579ed
Kevin Konczak <konczakkevin@gmail.com>
2021-08-13 11:20:42 +0900

    updating files before pushing to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
   resources :lists, only: %i[index new show create destroy] do
     resources :bookmarks, only: %i[new create destroy]
   end
